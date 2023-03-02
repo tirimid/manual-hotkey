52f3a75eddff66755eb6bce6f850d79fa7ccbf7b
dmfedorin <m7996@students.eischools.ae>
2023-03-02 08:28:19 +0400

    setup for parser done

--- inc/lang/parse.h
+++ inc/lang/parse.h
@@ -1,10 +1,14 @@
 #ifndef PARSE_H__
 #define PARSE_H__
 
+#include "lang/lex.h"
 #include "util/dynarr.h"
 
 enum node_type {
+    // symbolizes the root node of an AST.
+    // this type of node is returned from `parse()`.
     NODE_TYPE_ROOT,
+    
     NODE_TYPE_FUNCTION,
     NODE_TYPE_REGISTRY,
     NODE_TYPE_BLOCK,
@@ -23,11 +27,21 @@ enum node_type {
     NODE_TYPE_EXPR_BINOP_AND,
     NODE_TYPE_EXPR_BINOP_OR,
     NODE_TYPE_EXPR_BINOP_ASSIGN,
+
+    NODE_TYPE_LAST__, // see `TOKEN_TYPE_LAST__` in `lang/lex.h`.
 };
 
 struct node {
     enum node_type type;
-    struct dynarr children, tokens;
+    struct dynarr children, toks;
 };
+
+struct node node_create(enum node_type type);
+void node_destroy(struct node *n);
+void node_add_child(struct node *n, struct node const *child);
+void node_add_token(struct node *n, struct token const *tok);
+void node_print(struct node const *n);
+
+struct node parse(struct dynarr const *toks);
 
 #endif
--- src/lang/parse.c
+++ src/lang/parse.c
@@ -1,1 +1,78 @@
 #include "lang/parse.h"
+
+#include <stddef.h>
+#include <stdio.h>
+
+static char const *node_type_names[NODE_TYPE_LAST__] = {
+    "root",
+    "function",
+    "registry",
+    "block",
+    "variable",
+    "expression",
+    "expr_function_call",
+    "expr_variable_lookup",
+    "expr_unop_negate",
+    "expr_binop_power",
+    "expr_binop_multiply",
+    "expr_binop_divide",
+    "expr_binop_add",
+    "expr_binop_subtract",
+    "expr_binop_and",
+    "expr_binop_or",
+    "expr_binop_assign",
+};
+
+struct node node_create(enum node_type type)
+{
+    return (struct node){
+        .type = type,
+        .children = dynarr_create(sizeof(struct node)),
+        .toks = dynarr_create(sizeof(struct token)),
+    };
+}
+
+void node_destroy(struct node *n)
+{
+    for (size_t i = 0; i < n->children.size; ++i)
+        node_destroy(dynarr_get_mut(&n->children, i));
+
+    dynarr_destroy(&n->children);
+    destroy_token_dynarr(&n->toks);
+}
+
+void node_add_child(struct node *n, struct node const *child)
+{
+    dynarr_push(&n->children, child);
+}
+
+void node_add_token(struct node *n, struct token const *tok)
+{
+    dynarr_push(&n->toks, tok);
+}
+
+static void node_print_real(struct node const *n, size_t depth)
+{
+    for (size_t i = 0; i < depth; ++i)
+        putc('\t', stdout);
+
+    printf("%s%s [ ", depth >= 1 ? "| " : "", node_type_names[n->type]);
+    for (size_t i = 0; i < n->toks.size; ++i) {
+        struct token const *tok = dynarr_get(&n->toks, i);
+        printf("'%s' (%d) ", tok->conts, tok->type);
+    }
+    puts("]\n");
+
+    for (size_t i = 0; i < n->children.size; ++i)
+        node_print_real(dynarr_get(&n->children, i), depth + 1);
+}
+
+void node_print(struct node const *n)
+{
+    node_print_real(n, 0);
+}
+
+struct node parse(struct dynarr const *toks)
+{
+    return node_create(NODE_TYPE_ROOT);
+}
